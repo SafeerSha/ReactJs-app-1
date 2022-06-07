de11712a24eca9276f652477dce1c5cc97d200ef
SafeerSha <pksafeer8@gmail.com>
2022-06-07 21:42:16 +0530

    First Design using reactjs

---
+++ src/Data.js
@@ -0,0 +1,86 @@
+export const sliderItems = [
+    {
+        id : 1,
+        img : "https://freepngimg.com/thumb/shopping/9-2-shopping-download-png.png",
+        title : "SUMMER SALE!",
+        desc: "Summer sale is on now ",
+        bg : "f5fafd"
+    },
+    {
+        id : 2,
+        img : "https://freepngimg.com/thumb/shopping/6-2-shopping-transparent.png",
+        title : "MAEGA SALE!",
+        desc: "Mega sale is on now ",
+        bg : "fcf1ed"
+    },
+    {
+        id : 3,
+        img : "https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525086721fosbj.png",
+        title : "WINTER SALE!",
+        desc: "Winter sale is on now ",
+        bg : "f5fafd"
+    },
+]
+
+export const categories = [
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/297933/pexels-photo-297933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"SHIRT STYLES",
+
+    },
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/1082528/pexels-photo-1082528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"JEANS PANTS"
+    },
+    {
+        id:3,
+        img:"https://img.abercrombie.com/is/image/anf/KIC_144-2156-1346-178_prod1?policy=product-medium",
+        title:"LIGHT JACKETS",
+    },
+];
+
+export const PopularProducts = [
+    {
+        id:1,
+        img:"https://rukminim1.flixcart.com/image/800/960/l2dmky80/jean/v/d/p/32-udjeno0513-u-s-polo-association-original-imagdqeykcveah2p.jpeg?q=50",
+
+    },
+    {
+        id:2,
+        img:"https://rukminim1.flixcart.com/image/496/595/kc9eufk0/short/s/e/u/xxl-g-mshort-navy-an-mil-2pcs-gyrfalcon-original-imaftfkzzxh7tj7g.jpeg?q=50",
+
+    },
+    {
+        id:3,
+        img:"https://rukminim1.flixcart.com/image/496/595/kxkqavk0/t-shirt/m/x/n/m-fc4195-1-fastcolors-original-imagay6kzzk3eah7.jpeg?q=50",
+
+    },
+    {
+        id:4,
+        img:"https://rukminim1.flixcart.com/image/496/595/ksaoqkw0/t-shirt/q/5/w/l-fc4162-fastcolors-original-imag5w3f8dnpzymw.jpeg?q=50",
+
+    },
+    {
+        id:5,
+        img:"https://rukminim1.flixcart.com/image/496/595/klphn680/top/i/y/j/xl-regular-top-fab-star-original-imagyry2fm84svc2.jpeg?q=50",
+
+    },
+    {
+        id:6,
+        img:"https://rukminim1.flixcart.com/image/496/595/km2clu80/jacket/c/f/0/s-mnt-7003-montrez-original-imagffv4hs3k6quc.jpeg?q=50",
+
+    },
+    {
+        id:7,
+        img:"https://rukminim1.flixcart.com/image/496/595/ku5ufm80/ethnic-set/x/b/f/s-1135-golden-printed-reyon-anarkali-suit-in-dusty-blue-new-original-imag7ccegya27328.jpeg?q=50",
+
+    },
+    {
+        id:8,
+        img:"https://rukminim1.flixcart.com/image/800/960/kt7jv680/track-pant/y/u/z/m-gt5567-adidas-original-imag6hwssfmhycsg.jpeg?q=50",
+    },
+    
+    
+]
---
+++ src/components/Products.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { PopularProducts } from "../Data";
+import Product from "./Product";
+
+
+const Container = styled.div`
+padding:20px;
+display:flex;
+flex-wrap:wrap;
+justify-content:space-between;
+`
+
+
+
+const Products = () => {
+  return (
+    <Container>
+        {PopularProducts.map((item) => (
+            <Product item={item} key={item.id}/>
+
+        ))}
+        
+
+    </Container>
+  )
+};
+
+export default Products
